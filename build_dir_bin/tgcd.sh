df309fd7dc56a6749502a64bd027fa4d3f5189ea
Brian Flannery <bflannery@caci.com>
2016-04-25 19:37:19 -0600

    Script to run tgcd

---
+++ build_dir_bin/tgcd.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# # # USAGE: tgcd.sh $jnlpPort        # for server-server mode
+# # #    OR: tgcd.sh $jnlpPort client # for client-client mode
+# # #    OR: tgcd.sh ''        $mode  # for default port
+
+sleepy=10 ;
+what=tgcd ;
+jnlpPort=$1 ;
+which=$2 ;
+[[ $which ]] || which=server ;
+which="$which-$which" ;
+[[ $jnlpPort ]] || jnlpPort=44422 ;
+nextPort=$((jnlpPort + 1)) ;
+if [[ "$which" == "server-server" ]] ; then
+  mode='-L' ;
+  flag1='-p' ;
+  arg1="$jnlpPort" ;
+  flag2='-q' ;
+  arg2="$nextPort" ;
+else
+  mode='-C' ;
+  flag1='-s' ;
+  arg1="localhost:$jnlpPort" ;
+  flag2='-c' ;
+  arg2="jenkins-jnlp.$domain:$nextPort" ;
+fi ;
+pwd ;
+id ;
+nohup /home/$USER/app/bin/$what $mode $flag1 $arg1 $flag2 $arg2 \
+  & echo "Backgrounding '$what $mode $flag1 $arg1 $flag2 $arg2'." ;
+sleep $sleepy ;
+#
+
+stillUp() {
+  x=`ps -ef | egrep -v egrep | egrep -v "$what[.]sh" | egrep $what ` ;
+  [[ $x ]] || echo "ERROR: Lost $what process." ;
+  [[ $x ]] && sleep $sleepy || exit 1 ;
+}
+
+while 'true' ; do
+  stillUp ;
+done ;
+
+#
