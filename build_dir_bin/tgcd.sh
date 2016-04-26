8cfc9ebf9b5c35df7b3c73a8187a65ba3338b41c
Brian Flannery <bflannery@caci.com>
2016-04-26 10:10:06 -0600

    Add debug clarity to tgcd.sh

--- build_dir_bin/tgcd.sh
+++ build_dir_bin/tgcd.sh
@@ -4,6 +4,7 @@
 # # #    OR: tgcd.sh $jnlpPort client # for client-client mode
 # # #    OR: tgcd.sh ''        $mode  # for default port
 
+DEBUG='1' ; # Blank ('') for no debug output.
 sleepy=10 ;
 what=tgcd ;
 jnlpPort=$1 ;
@@ -25,8 +26,12 @@ else
   flag2='-c' ;
   arg2="jenkins-jnlp.$domain:$nextPort" ;
 fi ;
+if [[ $DEBUG ]] ; then
+echo "DEBUG: In $0:" ;
 pwd ;
 id ;
+echo ;
+fi ;
 nohup /home/$USER/app/bin/$what $mode $flag1 $arg1 $flag2 $arg2 \
   & echo "Backgrounding '$what $mode $flag1 $arg1 $flag2 $arg2'." ;
 sleep $sleepy ;
