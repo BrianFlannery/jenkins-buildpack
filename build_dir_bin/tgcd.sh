fe3f5d955e2edc6a0fcf0d47bb1062cf2a08c081
Brian Flannery <bflannery@caci.com>
2016-04-27 11:24:20 -0600

    Update tgcd.sh

--- build_dir_bin/tgcd.sh
+++ build_dir_bin/tgcd.sh
@@ -4,7 +4,12 @@
 # # #    OR: tgcd.sh $jnlpPort client # for client-client mode
 # # #    OR: tgcd.sh ''        $mode  # for default port
 
-DEBUG='1' ; # Blank ('') for no debug output.
+DEBUG=$DEBUG_JJ ; # Zero (0) for no debug output.
+[[ $DEBUG ]] || DEBUG=0 ;
+logFileFlag='-l' ;
+logFileArg='/tmp/log.tgcd.sh.txt' ;
+logLevelFlag='-g' ;
+logLevelArg='3' ;
 sleepy=10 ;
 what=tgcd ;
 jnlpPort=$1 ;
@@ -15,6 +20,7 @@ which="$which-$which" ;
 [[ $server ]] || server=$JNLP_APP_IP ;
 [[ $jnlpPort ]] || jnlpPort=$JENKINS_SLAVE_AGENT_PORT ;
 [[ $jnlpPort ]] || jnlpPort=44422 ;
+# [[ $nextPort ]] || nextPort=$JENKINS_SLAVE_AGENT_PORT_EXT ;
 [[ $nextPort ]] || nextPort=$JENKINS_SLAVE_AGENT_JNLP_PORT ;
 [[ $nextPort ]] || nextPort=$((jnlpPort + 1)) ;
 if [[ "$which" == "server-server" ]] ; then
@@ -34,20 +40,40 @@ else
   flag3='-i' ;
   arg3='10' ;
 fi ;
-if [[ $DEBUG ]] ; then
+if [[ $DEBUG -gt 0 ]] ; then
   echo "DEBUG: In $0:" ;
   pwd ;
   id ;
   echo ;
 fi ;
-nohup /home/$USER/app/bin/$what $mode $flag1 $arg1 $flag2 $arg2 $flag3 $arg3 -m f \
-  & echo "Backgrounding '$what $mode $flag1 $arg1 $flag2 $arg2 $flag3 $arg3 -m f '." ;
+# nohup /home/$USER/app/bin/$what $mode $flag1 $arg1 $flag2 $arg2 $flag3 $arg3 -m f \
+#   & echo "Backgrounding '$what $mode $flag1 $arg1 $flag2 $arg2 $flag3 $arg3 -m f '." ;
+nohup /home/$USER/app/bin/$what $mode $flag1 $arg1 $flag2 $arg2 $flag3 $arg3 -m f $logFileFlag $logFileArg $logLevelFlag $logLevelArg \
+  & echo "Backgrounding '$what $mode $flag1 $arg1 $flag2 $arg2 $flag3 $arg3 -m f $logFileFlag $logFileArg $logLevelFlag $logLevelArg '." ;
 sleep $sleepy ;
 #
 
 stillUp() {
   x=`ps -ef | egrep -v egrep | egrep -v "$what[.]sh" | egrep $what ` ;
+  if [[ $DEBUG -gt 1 || -z $x ]] ; then
+    echo ;
+    date ;
+    /sbin/ifconfig | grep inet ;
+    ps -ef | egrep -v grep | grep -v diego ;
+    free ;
+    echo ;
+    echo -n 'x preview: ' ;
+    ps -ef | egrep -v egrep | egrep -v "$what[.]sh" ;
+    echo ;
+    echo "x = '$x'" ;
+    echo -e '\n' ;
+  fi ;
   [[ $x ]] || echo "ERROR: Lost $what process." ;
+  if [[ $DEBUG -gt 0 || -z $x ]] ; then
+    echo "Tail $what logs:" ;
+    tail $logFileArg ;
+  fi ;
+  [[ $x ]] || (echo "Another ps: " ; ps -ef | egrep -v grep | grep -v diego ; echo ) ;
   [[ $x ]] && sleep $sleepy || exit 1 ;
 }
 
